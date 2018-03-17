5c42fa35091f7fa46f07e0491893a3c842c6eea3
niks8693 <nikhil.shende2018@gmail.com>
2018-03-17 09:31:21 -0500

    currency-exchange-service created

---
+++ src/main/java/com/niks/springcloud/currencyexchangeservice/CurrencyExchangeController.java
@@ -0,0 +1,24 @@
+package com.niks.springcloud.currencyexchangeservice;
+
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CurrencyExchangeController {
+	
+	@Autowired
+	private Environment env;
+	
+	@GetMapping("currency-exchange/from/{from}/to/{to}")
+	public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to){
+		ExchangeValue exValue=new ExchangeValue(10L,from,to,BigDecimal.valueOf(65));
+		exValue.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+		return  exValue;
+	}
+
+}
